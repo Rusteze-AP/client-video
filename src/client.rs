d3e5a6aec69b7514134c2a38450b5a5f74fe0e26
StefanoSacchet <sacchet13@gmail.com>
2024-12-22 17:49:25 +0100

    added crash handler and more

--- src/client.rs
+++ src/client.rs
@@ -1,24 +1,28 @@
-use crossbeam::channel::{Receiver, Sender, TryRecvError};
-use packet_forge::{PacketForge, TextMessage};
+use crossbeam::channel::{select_biased, Receiver, Sender};
+use packet_forge::PacketForge;
+use rocket::{self, Build, Ignite, Rocket, State};
 use std::collections::HashMap;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::time::Duration;
 use wg_internal::controller::{DroneCommand, DroneEvent};
 use wg_internal::network::NodeId;
 use wg_internal::packet::Packet;
 
-use rocket::{self, Build, Ignite, Rocket, State};
+#[derive(Debug)]
+pub struct ClientState {
+    id: NodeId,
+    controller_send: Sender<DroneEvent>,
+    controller_recv: Receiver<DroneCommand>,
+    packet_recv: Receiver<Packet>,
+    senders: HashMap<NodeId, Sender<Packet>>,
+    packet_forge: PacketForge,
+    terminated: bool,
+}
 
 #[derive(Debug, Clone)]
 pub struct Client {
-    id: NodeId,
-    command_send: Sender<DroneEvent>,
-    command_recv: Receiver<DroneCommand>,
-    receiver: Receiver<Packet>,
-    senders: HashMap<NodeId, Sender<Packet>>,
-    packet_forge: PacketForge,
+    state: Arc<RwLock<ClientState>>,
 }
 
 impl Client {
@@ -30,87 +34,108 @@ impl Client {
         receiver: Receiver<Packet>,
         senders: HashMap<NodeId, Sender<Packet>>,
     ) -> Self {
-        Client {
+        let state = ClientState {
             id,
-            command_send,
-            command_recv,
-            receiver,
+            controller_send: command_send,
+            controller_recv: command_recv,
+            packet_recv: receiver,
             senders,
             packet_forge: PacketForge::new(),
+            terminated: false,
+        };
+
+        Client {
+            state: Arc::new(RwLock::new(state)),
         }
     }
 
-    pub fn get_id(&self) -> NodeId {
-        self.id
+    fn command_dispatcher(&self, command: &DroneCommand) {
+        let mut state = self.state.write().unwrap();
+
+        match command {
+            DroneCommand::Crash => {
+                state.terminated = true;
+            }
+            DroneCommand::SetPacketDropRate(_) => {
+                eprintln!(
+                    "Client {}, error: received a SetPacketDropRate command",
+                    state.id
+                );
+            }
+            _ => {
+                eprintln!(
+                    "Client {}, error: received an unknown command: {:?}",
+                    state.id, command
+                );
+            }
+        }
     }
 
-    /// Runs the client's message processing in a separate thread
-    pub fn start_message_processing(mut self, running: Arc<AtomicBool>) -> thread::JoinHandle<()> {
+    #[must_use]
+    pub fn start_message_processing(self) -> thread::JoinHandle<()> {
+        let state = self.state.clone();
+
         thread::spawn(move || {
-            while running.load(Ordering::SeqCst) {
+            loop {
                 thread::sleep(Duration::from_secs(1));
 
-                // Check if there's a message
-                match self.receiver.try_recv() {
-                    Ok(packet) => {
-                        println!("Client {} received a message: {:?}", self.id, packet);
+                // Get mutable access to state
+                let mut state_guard = state.write().unwrap();
+
+                if state_guard.terminated {
+                    break;
+                }
+
+                select_biased! {
+                    recv(state_guard.controller_recv) -> command => {
+                        if let Ok(command) = command {
+                            println!("Client {} received a command: {:?}", state_guard.id, command);
+                            self.command_dispatcher(&command);
+                        } else {
+                            println!("Client {}, SC disconnected", state_guard.id);
+                            break;
+                        }
                     }
-                    Err(TryRecvError::Empty) => {
-                        println!("No messages for client {}", self.id);
-                    }
-                    Err(err) => {
-                        eprintln!("Error receiving message for client {}: {:?}", self.id, err);
-                        break;
+                    recv(state_guard.packet_recv) -> msg => {
+                        if let Ok(msg) = msg {
+                            println!("Client {} received a message: {:?}", state_guard.id, msg);
+                            if state_guard.id == 20 {
+                                state_guard.id = 69;
+                            } else {
+                                state_guard.id = 20;
+                            }
+                        } else {
+                            eprintln!(
+                                "Error receiving message for client {}", state_guard.id);
+                            break;
+                        }
                     }
                 }
 
-                // Message sending logic
-                let text_msg =
-                    TextMessage::new("c".repeat(128), String::from("20"), String::from("30"));
-                if let Ok(packets) = self.packet_forge.disassemble(&text_msg, vec![20, 1, 30]) {
-                    for packet in packets {
-                        let id = 1;
-                        if let Some(sender) = self.senders.get(&id) {
-                            if let Err(err) = sender.send(packet) {
-                                eprintln!("Error sending packet to node {id}: {err:?}");
-                            } else {
-                                println!("Client {} sent packet to node {}", self.id, id);
-                            }
-                        } else {
-                            println!("Client {} could not send packet to node {}", self.id, id);
-                        }
-                    }
-                } else {
-                    eprintln!("Error disassembling message: {text_msg:?}");
-                }
+                // RwLock is automatically released here when state_guard goes out of scope
             }
         })
     }
 
-    /// Configures the Rocket web server
+    #[must_use]
     pub fn configure(client: Client) -> Rocket<Build> {
         rocket::build()
-            .manage(client) // Manage the client state
-            .mount("/", routes![index, client_info])
+            .manage(client)
+            .mount("/", routes![client_info])
     }
 
-    /// Launches the Rocket web server and starts message processing
-    pub async fn run(self, running: Arc<AtomicBool>) -> Result<Rocket<Ignite>, rocket::Error> {
-        // Start message processing in a separate thread
-        let _processing_handle = self.clone().start_message_processing(running);
-
-        // Launch Rocket server
+    /// Launch the Rocket app
+    /// This function will block the current thread until the Rocket app is shut down
+    /// # Errors
+    /// If the Rocket app fails to launch
+    pub async fn run(self) -> Result<Rocket<Ignite>, rocket::Error> {
+        let _processing_handle = self.clone().start_message_processing();
         Self::configure(self).launch().await
     }
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-/// Client info endpoint
 #[get("/client-info")]
 fn client_info(client: &State<Client>) -> String {
-    format!("Client ID: {}", client.get_id())
+    let state = client.state.read().unwrap();
+    format!("Client ID: {}", state.id)
 }
