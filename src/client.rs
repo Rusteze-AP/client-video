60788fbc42c62fb58289aeb0f06d377a027333fb
StefanoSacchet <sacchet13@gmail.com>
2025-01-02 18:09:31 +0100

    handled panics

--- src/client.rs
+++ src/client.rs
@@ -71,12 +71,30 @@ impl Client {
 
     fn request_video(&self, video_name: &str) {
         let mut state = self.state.write().unwrap();
+
         let msg = ChunkRequest::new(video_name.to_string(), Index::All);
-        let packets = state
-            .packet_forge
-            .disassemble(msg, vec![20, 1, 30])
-            .unwrap();
-        let sender = state.senders.get(&1).unwrap();
+        let hops = vec![20, 1, 30];
+        let dest = hops[1];
+
+        // Disassemble the message into packets
+        let packets = state.packet_forge.disassemble(msg, hops);
+        if packets.is_err() {
+            eprintln!("Client {}, error: disassembling failed", state.id);
+            return;
+        }
+        let packets = packets.unwrap();
+
+        let state = self.state.read().unwrap();
+
+        // Get sender
+        let sender = state.senders.get(&dest);
+        if sender.is_none() {
+            eprintln!("Client {}, error: sender {} not found", state.id, dest);
+            return;
+        }
+        let sender = sender.unwrap();
+
+        // Send packets
         for packet in packets {
             sender.send(packet).unwrap();
         }
